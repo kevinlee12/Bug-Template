5b80a7ef71ff53aad7b259bc288edfb7e3aa5165
Kevin Lee <leekevin@berkeley.edu>
2015-03-30 13:36:21 -0700

    Condensed code

--- ReportTemplates.py
+++ ReportTemplates.py
@@ -7,10 +7,11 @@ def reporting():
     print("Select the type of reporting:")
     print("1. Bug report")
     print("2. Enhancement report")
-    type = input("Report type: ")
-    if type == "1":
+    report_type = input("Report type: ")
+    assert type(report_type) == str, "You are not using Python 3!"
+    if report_type == "1":
         bug_report()
-    elif type == "2":
+    elif report_type == "2":
         enhancement_template()
     else:
         print("Not a valid selection")
@@ -29,31 +30,26 @@ def bug_report():
     print("3) Feature Bug")
     print("4) Server error")
     print("5) Other bug")
-    type = input("Bug type is: ")
-    if type == "1":
-        bug_type = "Graphical bug"
-    elif type == "2":
-        bug_type = "Security bug"
-    elif type == "3":
-        bug_type = "Feature bug"
-    elif type == "4":
+    bug_type = input("\nBug type is: ")
+    if bug_type == "1":
+        bug_is = "Graphical bug"
+    elif bug_type == "2":
+        bug_is = "Security bug"
+    elif bug_type == "3":
+        bug_is = "Feature bug"
+    elif bug_type == "4":
         server_error = input("Server error code is: ")
-        bug_type = "Server error " + server_error
+        bug_is = "Server error " + server_error
     else:
-        bug_type = "Other bug"
+        bug_is = "Other bug"
     screenshot_present = input("Include Screenshot? (Y/n):")
-    if screenshot_present == "Y" or screenshot_present == "y":
-        include_screenshot = True
-    else:
-        include_screenshot = False
-    print("\n \n<h5>Type: " + bug_type + "</h5> \n")
-    print("<h4>Description:</h4> \n...\n")
-    print("<h4>Machine Info:</h4> \nOS: Ubuntu 14.04.2 LTS \nBrowser: Google Chrome (64-bit)")
-    print("<h5>Expected output:</h5> \n ... \n<h5>Actual output:</h5> \n ...")
+    print("\n \n" + report_typing("Bug", bug_is) + "\n")
+    print(description() + "\n")
+    print(machine_info())
+    print("\n<h5>Expected output:</h5> \n ... \n<h5>Actual output:</h5> \n ...")
     print("\n<h4>Steps to recreate:</h4>\n1. \n2. \n3. \n")
     print("\n<h4>Additional information:</h4> \n ....\n")
-    if include_screenshot:
-        print("<h4>Screenshots:</h4> \n ...")
+    print(screenshot(screenshot_present))
 
 def enhancement_template():
     """Enhancement report template generator"""
@@ -61,23 +57,35 @@ def enhancement_template():
     print("1. User Flow")
     print("2. Graphical")
     print("3. Security")
-    type = input("Enhancement type is: ")
-    if type == "1":
-        enhancement_type = "User Flow"
-    elif type == "2":
-        enhancement_type = "Graphical"
-    elif type == "3":
-        enhancement_type = "Security"
+    enhancement_type = input("\nEnhancement type is: ")
+    if enhancement_type == "1":
+        enhancement_is = "User Flow"
+    elif enhancement_type == "2":
+        enhancement_is = "Graphical"
+    elif enhancement_type == "3":
+        enhancement_is = "Security"
     else:
-        enhancement_type = "Other"
+        enhancement_is = "Other"
     screenshot_present = input("Include Screenshot? (Y/n)")
-    if screenshot_present == "Y" or screenshot_present == "y":
-        include_screenshot = True
-    else:
-        include_screenshot = False
-    print("\n \n<b>Type: " + enhancement_type + " Enhancement</b>\n")
-    print("<h4>Description</h4> \n ...")
-    if include_screenshot:
-        print("\n<h4>Screenshot</h4> \n ...")
+    print("\n \n" + report_typing("Enhancement", enhancement_is) + "\n")
+    print(description())
+    print(screenshot(screenshot_present))
+
+def description():
+    return "<h4>Description</h4> \n ..."
+
+def machine_info():
+    device_info = "<h4>Machine Info:</h4>"
+    device_info += "\nOS: Ubuntu 14.04.2 LTS"
+    device_info += "\nBrowser: Google Chrome (64-bit)"
+    return device_info
+
+def screenshot(include):
+    if include == "y" or include == "Y":
+        return "<h4>Screenshot</h4> \n ..."
+    return
+
+def report_typing(bug_or_enhancement, specified_type):
+    return "<b>Type: " + specified_type + " " + bug_or_enhancement + "</b>"
 
 reporting()
